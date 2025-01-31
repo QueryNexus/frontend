bc063fc7049a39547c05ec1c6413fa303ee8f6e4
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-01-31 11:01:02 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Profile from './components/Profile.jsx';
+import Loading from './components/Loading.jsx';
 import './App.css';
 import authConfig from './config/auth_config.json';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <Auth0Provider
       domain={authConfig.domain}
---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../styles/Loading.css';
+
+function Loading() {
+  return (
+    <div className="loading">
+      <p className='first'>QueryNexus.com</p>
+      <p className='second'>Just Resolve It !!</p>
+    </div>
+  );
+}
+
+export default Loading;
