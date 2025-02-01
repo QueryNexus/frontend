d634800ea388413bd5348e8c55de27120d728c69
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-02-01 19:43:56 +0530

    restyles auth0 box

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "./../styles/MainPage.css";
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <div>
+      <div className="top">
+        <div className="user-logo">
+          <img src="" alt="" />
+        </div>
+      </div>
+
+      <div className="bottom">
+        <p>My Websites</p>
+        <ul>
+          <li>My Website url</li>
+          <li>My Website url</li>
+          <li>My Website url</li>
+          <li>My Website url</li>
+          <li>My Website url</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
