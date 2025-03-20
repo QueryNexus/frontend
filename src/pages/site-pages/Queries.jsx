dfd71c5ac9ac1e06eb7e7aa3b4fdcdda8b3e1f0b
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-03-20 15:40:43 +0530

    Checkpoint 1

--- src/pages/site-pages/Queries.jsx
+++ src/pages/site-pages/Queries.jsx
@@ -7,63 +7,63 @@ import { useParams } from 'react-router-dom';
 import Loader from './../../components/Loader';
 
 function Queries() {
-  const [queryData, setQueryData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const { companyId } = useParams();
+  // const [queryData, setQueryData] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const { companyId } = useParams();
 
-  const chartData = {
-    labels: ['Critical, Solved, Unsolved'],
-    datasets: [
-      {
-        label: '# of Flags',
-        data: [10,10,10],
-        backgroundColor: ['rgb(255, 0, 0)', 'rgb(0, 247, 0)', 'rgb(4, 0, 255)'],
-        borderColor: ['rgb(255, 0, 0)', 'rgb(0, 247, 0)', 'rgb(4, 0, 255)'],
-        borderWidth: 1,
-      },
-    ],
-  };
+  // const chartData = {
+  //   labels: ['Critical, Solved, Unsolved'],
+  //   datasets: [
+  //     {
+  //       label: '# of Flags',
+  //       data: [10,10,10],
+  //       backgroundColor: ['rgb(255, 0, 0)', 'rgb(0, 247, 0)', 'rgb(4, 0, 255)'],
+  //       borderColor: ['rgb(255, 0, 0)', 'rgb(0, 247, 0)', 'rgb(4, 0, 255)'],
+  //       borderWidth: 1,
+  //     },
+  //   ],
+  // };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.post(`https://backend-snowy-mu.vercel.app/query/${companyId}`);
-        console.log('Query Data fetched successfully:', response.data);
-        setQueryData(response.data.history);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await axios.post(`https://backend-snowy-mu.vercel.app/query/${companyId}`);
+  //       console.log('Query Data fetched successfully:', response.data);
+  //       setQueryData(response.data.history);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
-  return (
-    loading ? (<Loader />) : (
-    <div className="queries-container">
-      <div className="top">
-        <Pie data={chartData} />
-      </div>
+  // return (
+  //   loading ? (<Loader />) : (
+  //   <div className="queries-container">
+  //     <div className="top">
+  //       <Pie data={chartData} />
+  //     </div>
       
-      <div className="bottom">
-        {loading ? (
-          <div className="loader">Loading...</div>
-        ) : (
-          queryData.map((item, index) => (
-            <div key={index} className="info-box">
-              <p><strong>Role:</strong> {item.role}</p>
-              <p><strong>Content:</strong> {item.content}</p>
-              <p><strong>Timestamp:</strong> {new Date(item.timestamp.$date).toLocaleString()}</p>
-            </div>
-          ))
-        )}
-      </div>
-    </div>
-    )
-  );
+  //     <div className="bottom">
+  //       {loading ? (
+  //         <div className="loader">Loading...</div>
+  //       ) : (
+  //         queryData.map((item, index) => (
+  //           <div key={index} className="info-box">
+  //             <p><strong>Role:</strong> {item.role}</p>
+  //             <p><strong>Content:</strong> {item.content}</p>
+  //             <p><strong>Timestamp:</strong> {new Date(item.timestamp.$date).toLocaleString()}</p>
+  //           </div>
+  //         ))
+  //       )}
+  //     </div>
+  //   </div>
+  //   )
+  // );
 }
 
 export default Queries;
